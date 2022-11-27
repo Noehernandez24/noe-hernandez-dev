a5b39a14da66c7408fb1ff994694e7b4bccbd893
noehernandez24 <evilld94@gmail.com>
2022-11-26 18:21:51 -0600

    portafolio 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const menuIcon = document.querySelector("#icono-menu"),
+menu = document.querySelector("#menu");
+
+const push = document.querySelector("#push");
+
+menuIcon.addEventListener('click', (e) => {
+    menu.classList.toggle('active');
+    push.classList.toggle("push")
+    
+})
