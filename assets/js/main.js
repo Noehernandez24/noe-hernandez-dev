90812da526e60e1c234e0a662d8029e67e685f86
noehernandez24 <evilld94@gmail.com>
2022-12-30 23:49:20 -0600

    ADD: Bar Animation Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ const menuIcon = document.querySelector("#icono-menu"),
 menu = document.querySelector("#menu");
 
 const push = document.querySelector("#push");
+const body = document.body
+const bar1 = document.getElementById("bar-1")
+const bar2 = document.getElementById("bar-2")
+const bar3 = document.getElementById("bar-3")
 
 menuIcon.addEventListener('click', (e) => {
     menu.classList.toggle('active');
@@ -9,3 +13,9 @@ menuIcon.addEventListener('click', (e) => {
     
 })
 
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+    bar1.classList.toggle("skills-animation-1", window.scrollY > 894 )
+    bar2.classList.toggle("skills-animation-2", window.scrollY > 894 )
+    bar3.classList.toggle("skills-animation-3", window.scrollY > 894 )
+})
